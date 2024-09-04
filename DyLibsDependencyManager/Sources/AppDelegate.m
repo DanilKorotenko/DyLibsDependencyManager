5f1f7706df4ca78ba5880017d843f6494b3f7798
Danil Korotenko <danil.korotenko@gmail.com>
2024-09-04 14:34:09 +0300

    Implemented opening files.

--- DyLibsDependencyManager/Sources/AppDelegate.m
+++ DyLibsDependencyManager/Sources/AppDelegate.m
@@ -13,19 +13,82 @@
 
 @implementation AppDelegate
 
-- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
-    // Insert code here to initialize your application
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+
 }
 
+- (void)applicationWillTerminate:(NSNotification *)aNotification
+{
 
-- (void)applicationWillTerminate:(NSNotification *)aNotification {
-    // Insert code here to tear down your application
 }
 
-
-- (BOOL)applicationSupportsSecureRestorableState:(NSApplication *)app {
+- (BOOL)applicationSupportsSecureRestorableState:(NSApplication *)app
+{
     return YES;
 }
 
+- (BOOL)applicationShouldTerminateAfterLastWindowClosed:(NSApplication *)sender
+{
+    return YES;
+}
 
+- (void)application:(NSApplication *)application openURLs:(NSArray<NSURL *> *)urls
+{
+    NSLog(@"urls: %@", urls);
+    for (NSURL *url in urls)
+    {
+        NSLog(@"url: %@", url);
+
+        BOOL isDirectory = NO;
+        NSFileManager *fileManager = [NSFileManager defaultManager];
+        [fileManager fileExistsAtPath:url.path isDirectory:&isDirectory];
+
+        if (isDirectory)
+        {
+            NSArray *urlResources = @[NSURLTypeIdentifierKey];
+            NSDirectoryEnumerationOptions options =
+                NSDirectoryEnumerationSkipsHiddenFiles |
+                NSDirectoryEnumerationSkipsPackageDescendants;
+            NSError *error = nil;
+
+            NSArray *dirURLs = [fileManager contentsOfDirectoryAtURL:url
+                includingPropertiesForKeys:urlResources options:options error:&error];
+            if (error)
+            {
+                [NSApp presentError:error];
+            }
+            else
+            {
+                [self application:NSApp openURLs:dirURLs];
+            }
+        }
+        else
+        {
+            NSError *error = nil;
+            NSString *urlType = nil;
+            [url getResourceValue:&urlType forKey:NSURLTypeIdentifierKey error:&error];
+
+            NSLog(@"urlType: %@",urlType);
+
+            if(error)
+            {
+                [NSApp presentError:error];
+            }
+            else
+            {
+                [[NSDocumentController sharedDocumentController] openDocumentWithContentsOfURL:url
+                    display:YES completionHandler:
+                        ^(NSDocument * _Nullable document, BOOL documentWasAlreadyOpen,
+                            NSError * _Nullable error)
+                        {
+                            if (error)
+                            {
+                                [NSApp presentError:error];
+                            }
+                        }];
+            }
+        }
+    }
+}
 @end
--- DyLibsDependencyManager/Sources/Document.m
+++ DyLibsDependencyManager/Sources/Document.m
@@ -13,39 +13,42 @@
 
 @implementation Document
 
-- (instancetype)init {
+- (instancetype)init
+{
     self = [super init];
-    if (self) {
+    if (self)
+    {
         // Add your subclass-specific initialization here.
     }
     return self;
 }
 
-+ (BOOL)autosavesInPlace {
++ (BOOL)autosavesInPlace
+{
     return YES;
 }
 
-
-- (NSString *)windowNibName {
+- (NSString *)windowNibName
+{
     // Override returning the nib file name of the document
     // If you need to use a subclass of NSWindowController or if your document supports multiple NSWindowControllers, you should remove this method and override -makeWindowControllers instead.
     return @"Document";
 }
 
-
-- (NSData *)dataOfType:(NSString *)typeName error:(NSError **)outError {
+- (NSData *)dataOfType:(NSString *)typeName error:(NSError **)outError
+{
     // Insert code here to write your document to data of the specified type. If outError != NULL, ensure that you create and set an appropriate error if you return nil.
     // Alternatively, you could remove this method and override -fileWrapperOfType:error:, -writeToURL:ofType:error:, or -writeToURL:ofType:forSaveOperation:originalContentsURL:error: instead.
     [NSException raise:@"UnimplementedMethod" format:@"%@ is unimplemented", NSStringFromSelector(_cmd)];
     return nil;
 }
 
-
-- (BOOL)readFromData:(NSData *)data ofType:(NSString *)typeName error:(NSError **)outError {
+- (BOOL)readFromData:(NSData *)data ofType:(NSString *)typeName error:(NSError **)outError
+{
     // Insert code here to read your document from the given data of the specified type. If outError != NULL, ensure that you create and set an appropriate error if you return NO.
     // Alternatively, you could remove this method and override -readFromFileWrapper:ofType:error: or -readFromURL:ofType:error: instead.
     // If you do, you should also override -isEntireFileLoaded to return NO if the contents are lazily loaded.
-    [NSException raise:@"UnimplementedMethod" format:@"%@ is unimplemented", NSStringFromSelector(_cmd)];
+//    [NSException raise:@"UnimplementedMethod" format:@"%@ is unimplemented", NSStringFromSelector(_cmd)];
     return YES;
 }
 
